b78b4a8ed6c45142cd99cc0c3ba567487622cdc3
DSEGroupdev <dan@dsegroupae.com>
2025-04-16 11:12:06 +0400

    Force redeploy with styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 // Trigger redeploy - April 16, 2024
+// Force redeploy with styles - April 16, 2024
 import Head from 'next/head'
 import { FiZap, FiTarget, FiLayers, FiTrendingUp, FiCheck, FiArrowRight, FiChevronDown, FiCopy } from 'react-icons/fi';
 import Image from 'next/image';
