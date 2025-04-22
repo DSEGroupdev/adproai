e6ba2d9b627bc94d2902993ff72cc0a2a3bc7189
DSEGroupdev <dan@dsegroupae.com>
2025-04-22 11:05:16 +0400

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['localhost'],
-    unoptimized: true
+    unoptimized: true,
   },
-  output: 'standalone'
+  output: 'export',
+  distDir: 'dist',
 }
 
 module.exports = nextConfig 
