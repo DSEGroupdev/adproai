1183f02a30eb6d763171eadad4597f6f4fa992f1
DSEGroupdev <dan@dsegroupae.com>
2025-04-25 07:42:58 +0400

    ✅ Add working Clerk auth integration

--- middleware.js
+++ middleware.js
@@ -2,9 +2,27 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api/generate"],
+  publicRoutes: [
+    "/",
+    "/api/generate",
+    "/sign-in",
+    "/sign-up",
+    "/favicon.svg",
+    "/logo.png",
+    "/_next/static/(.*)",
+    "/api/webhooks/(.*)"
+  ],
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: [
+    "/api/webhooks/(.*)"
+  ],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ],
 }; 
