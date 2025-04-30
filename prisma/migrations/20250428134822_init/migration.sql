cfa90b5befcef43a3d86cdb96a658ee2b633418a
DSEGroupdev <dan@dsegroupae.com>
2025-04-30 17:41:40 +0400

    fix: update migration to include clerkId column

--- prisma/migrations/20250428134822_init/migration.sql
+++ prisma/migrations/20250428134822_init/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "Plan" AS ENUM ('FREE', 'PRO');
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT,
+    "clerkId" TEXT NOT NULL,
     "plan" "Plan" NOT NULL DEFAULT 'FREE',
     "adsGenerated" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,6 +19,7 @@ CREATE TABLE "AdCopy" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "platform" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "AdCopy_pkey" PRIMARY KEY ("id")
@@ -26,6 +27,7 @@ CREATE TABLE "AdCopy" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
 
 -- AddForeignKey
 ALTER TABLE "AdCopy" ADD CONSTRAINT "AdCopy_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
